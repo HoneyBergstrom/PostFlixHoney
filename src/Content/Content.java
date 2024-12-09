2b4f3784b6751a5b959811e2253422f9b6d4dcf6
Honey Bergstrom <phueng03735@gmail.com>
2024-12-09 14:40:37 +0100

    setter and getter

--- src/Content/Content.java
+++ src/Content/Content.java
@@ -11,6 +11,60 @@ public abstract class Content {
     private boolean isAvailable ;
     private List<String> genre ;
 
+    public int getContentID() {
+        return contentID;
     }
 
+    public void setContentID(int contentID) {
+        this.contentID = contentID;
+    }
+
+    public String getTitel() {
+        return titel;
+    }
+
+    public void setTitel(String titel) {
+        this.titel = titel;
+    }
+
+    public String getDirector() {
+        return director;
+    }
+
+    public void setDirector(String director) {
+        this.director = director;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getReleaseYears() {
+        return releaseYears;
+    }
+
+    public void setReleaseYears(int releaseYears) {
+        this.releaseYears = releaseYears;
+    }
+
+    public boolean isAvailable() {
+        return isAvailable;
+    }
+
+    public void setAvailable(boolean available) {
+        isAvailable = available;
+    }
+
+    public List<String> getGenre() {
+        return genre;
+    }
+
+    public void setGenre(List<String> genre) {
+        this.genre = genre;
+    }
+}
 
