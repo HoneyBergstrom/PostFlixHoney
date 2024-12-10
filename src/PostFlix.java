3a8ebb65409a112e53d0fd4b2aaa9b29b700b1c1
hyper <123260045+KieranCode94@users.noreply.github.com>
2024-12-10 13:01:44 +0100

    Implemented main method

---
+++ src/PostFlix.java
@@ -0,0 +1,17 @@
+public class PostFlix {
+    public static void main(String[] args) {
+        new PostFlix();
+    }
+
+    public PostFlix() {
+        initContentManager();
+        run();
+    }
+    
+    private void initContentManager() {
+        
+    }
+
+    private void run() {
+    }
+}
